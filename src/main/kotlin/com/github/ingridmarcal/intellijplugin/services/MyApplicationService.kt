c703f8cabc1e9618a3f3833b6fe71efc509024c4
GitHub Action <action@github.com>
2021-08-01 19:38:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ingridmarcal/intellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ingridmarcal.intellijplugin.services
+
+import com.github.ingridmarcal.intellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
